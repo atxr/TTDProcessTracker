1bfd9bebfcbbd682391f280810713dd60e00237d
atxr <alexandre.tullot@protonmail.com>
2023-06-27 13:40:56 +0200

    Add empty usermode client

---
+++ Tracker/Tracker.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <Windows.h>
+#include "../TTDProcessTracker/TTDProcessTracker.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    PID_DATA test;
+    test.pid = 1;
+}
